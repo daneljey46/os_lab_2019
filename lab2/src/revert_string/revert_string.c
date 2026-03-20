45249fd1b5edc0fd4bb1188860e274b8aec17960
daneljey46 <daneljey46@gmail.com>
2026-03-20 22:57:36 +0000

    Задание 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,14 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);
+    int i, j;
+    char temp;
+
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
